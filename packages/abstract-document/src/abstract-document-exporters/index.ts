83331ac0c9e10b273768cb342142d021610f5a5a
Johan Kristiansson <johan1kristiansson@gmail.com>
2021-05-17 16:49:54 +0200

    Implemented DOCX

--- packages/abstract-document/src/abstract-document-exporters/index.ts
+++ packages/abstract-document/src/abstract-document-exporters/index.ts
@@ -1,4 +1,5 @@
 import * as Pdf from "./pdf/render";
+import * as Docx from "./docx2/render";
 // import * as Docx from "./docx/docx-document-renderer";
 
-export { Pdf /*Docx*/ };
+export { Pdf, Docx };
