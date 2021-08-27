bfec69abc63854231a5eac0e9ab2baa0b9af1720
Jonas Kello <jonas.kello@gmail.com>
2021-08-27 20:27:50 +0200

    Parse

--- packages/abstract-document/src/abstract-document-exporters/__tests__/pdf/export.test.tsx
+++ packages/abstract-document/src/abstract-document-exporters/__tests__/pdf/export.test.tsx
@@ -1,6 +1,6 @@
 import * as S from "stream";
 import path from "path";
-import pdf2json from "pdf2json";
+import PDFParser from "pdf2json";
 import PdfKit from "pdfkit";
 import { loadTests, onlySkip, streamToBuffer } from "@abstract-visuals/test-utils";
 import { exportToStream } from "../../../abstract-document-exporters/pdf/render";
@@ -16,7 +16,23 @@ describe("export docx", () => {
       const pdfStream = new S.PassThrough();
       exportToStream(PdfKit, pdfStream, abstractDoc);
       const pdfBuffer = await streamToBuffer(pdfStream);
+      const parsed = await getJsonFromPdf(pdfBuffer);
       console.log("item", item);
+      console.log("parsed", parsed);
     });
   });
 });
+
+function getJsonFromPdf(pdfBuffer: Buffer): Promise<unknown> {
+  return new Promise((resolve, reject) => {
+    let pdfParser = new PDFParser();
+    pdfParser.on("pdfParser_dataError", (errData: { parserError: string }) => {
+      reject(errData);
+    });
+    pdfParser.on("pdfParser_dataReady", (pdfData: unknown) => {
+      console.log(JSON.stringify(pdfData));
+      resolve(pdfData);
+    });
+    pdfParser.parseBuffer(pdfBuffer);
+  });
+}
