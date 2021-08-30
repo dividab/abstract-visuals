c638a82e0ee8278ea379917b20420510a543eca3
Jonas Kello <jonaskello@users.noreply.github.com>
2021-08-30 09:48:58 +0200

    chore(tests): Refactor tests (#35)
    * Initial commit
    
    * Compiles
    
    * Begin data tests
    
    * Rename test folders
    
    * Refactor folders
    
    * Load tests
    
    * Cleanup
    
    * Cleanup
    
    * Move stories
    
    * Exclude stories
    
    * Fix build
    
    * xml diff
    
    * Use wildcards
    
    * Use package for tst utils
    
    * Add pdf tests
    
    * Parse
    
    * Pdf tests
    
    * Add script
    
    * Save files
    
    * Pass
    
    * Run all tests
    
    * Upgrade jest
    
    * Upgrade eslint
    
    * Split workflow
    
    * Rename file
    
    * Rename workflow
    
    * Fix some linting
    
    * Split ci jobs

--- .storybook/config.js
+++
@@ -1,9 +0,0 @@
-import { configure } from "@storybook/react";
-
-function loadStories() {
-  require("../packages/_stories/lib");
-  // require("../packages/_stories/lib/react-properties-selector");
-  // require("../packages/_stories/lib/react-property-selectors");
-}
-
-configure(loadStories, module);
