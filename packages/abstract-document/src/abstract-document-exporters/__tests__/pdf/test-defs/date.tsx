52d5b38fc20704b78694a235712ba3eef2373491
Oskar Dahlin <oskar.dahlin@divid.se>
2021-09-01 15:01:01 +0200

    Added date test for pdf

---
+++ packages/abstract-document/src/abstract-document-exporters/__tests__/pdf/test-defs/date.tsx
@@ -0,0 +1,66 @@
+import React from "react";
+import { ExportTestDef } from "../export-test-def";
+import * as AD from "../../../../index";
+import { Paragraph, AbstractDoc, Section, TextField } from "../../../../abstract-document-jsx";
+
+export const test: ExportTestDef = {
+  name: "Single date",
+  abstractDocJsx: (
+    <AbstractDoc>
+      <Section>
+        <Paragraph>
+          <TextField fieldType="Date" />
+        </Paragraph>
+      </Section>
+    </AbstractDoc>
+  ),
+  expectedPdfJson: {
+    formImage: {
+      Agency: "",
+      Id: {
+        AgencyId: "",
+        MC: false,
+        Max: 1,
+        Name: "",
+        Parent: "",
+      },
+      Pages: [
+        {
+          Boxsets: [],
+          Fields: [],
+          Fills: [
+            {
+              clr: 1,
+              h: 0,
+              w: 0,
+              x: 0,
+              y: 0,
+            },
+          ],
+          HLines: [],
+          Height: 52.625,
+          Texts: [
+            {
+              A: "left",
+              R: [
+                {
+                  S: -1,
+                  T: encodeURIComponent(new Date(Date.now()).toDateString()),
+                  TS: [0, 13, 0, 0],
+                },
+              ],
+              clr: 0,
+              sw: 0.32553125,
+              w: "*",
+              x: -0.25,
+              y: -0.301,
+            },
+          ],
+          VLines: [],
+        },
+      ],
+      Transcoder: "pdf2json@1.2.3 [https://github.com/modesty/pdf2json]",
+      Width: 37.188,
+    },
+  },
+};
