18f5f225cf5d4210fa1a9b00ef1c61a92c2145ff
xpopy <3540971+xpopy@users.noreply.github.com>
2021-09-07 13:12:15 +0200

    Add undefined color tests

--- packages/abstract-image/src/model/__tests__/color/export-test-def.ts
+++ packages/abstract-image/src/model/__tests__/color/export-test-def.ts
@@ -2,10 +2,12 @@
  * Defines a data-type that represents a single test for the Docx render function
  */
 
+import * as AbstractImage from "../../../../src/index";
+
 export type ExportTestDef = {
   readonly name: string;
   readonly only?: boolean;
   readonly skip?: boolean;
-  readonly abstractColor: {};
-  readonly expectedColor: {};
+  readonly abstractColor: {} | AbstractImage.Color | undefined;
+  readonly expectedColor: {} | undefined;
 };
