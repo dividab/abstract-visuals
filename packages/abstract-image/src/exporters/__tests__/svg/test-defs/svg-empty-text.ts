c402706616ce333a1478a937d65d4fdd3219c82d
xpopy <3540971+xpopy@users.noreply.github.com>
2021-09-07 14:22:05 +0200

    Fix test name

--- packages/abstract-image/src/exporters/__tests__/svg/test-defs/svg-empty-text.ts
+++ packages/abstract-image/src/exporters/__tests__/svg/test-defs/svg-empty-text.ts
@@ -27,7 +27,7 @@ const image = AbstractImage.createAbstractImage(
 const svg = AbstractImage.createSVG(image);
 
 export const test: ExportTestDef = {
-  name: "svg text",
+  name: "svg empty text",
   abstractImage: svg,
   expectedImage: '<svg xmlns="http://www.w3.org/2000/svg" width="400px" height="400px" viewBox="0 0 400 400"></svg>',
 };
