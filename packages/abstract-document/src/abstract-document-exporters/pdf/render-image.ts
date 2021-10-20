34b3683721981aeda8f761d761f7aee0a54ef05b
Josef Dagson <josef.dagson@divid.se>
2021-10-20 09:21:15 +0200

    Using toLowerCase instead of Local version

--- packages/abstract-document/src/abstract-document-exporters/pdf/render-image.ts
+++ packages/abstract-document/src/abstract-document-exporters/pdf/render-image.ts
@@ -66,10 +66,10 @@ function abstractComponentToPdf(
             index = indexEnd;
 
             const color = svgUpdated.substring(indexStart, indexEnd);
-            if (color !== color.toLocaleLowerCase() && color.toLowerCase().indexOf("url") === -1)
+            if (color !== color.toLowerCase() && color.toLowerCase().indexOf("url") === -1)
               svgUpdated =
                 svgUpdated.substring(0, indexStart) +
-                color.toLocaleLowerCase() +
+                color.toLowerCase() +
                 svgUpdated.substring(indexEnd, svgUpdated.length);
           }
         });
