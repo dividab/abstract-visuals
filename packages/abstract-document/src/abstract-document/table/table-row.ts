fd12a3d5187b4cf5736a1c8af197b1dbcbdd85bf
xpopy <3540971+xpopy@users.noreply.github.com>
2021-11-04 15:28:45 +0100

    Set array back to ReadonlyArray

--- packages/abstract-document/src/abstract-document/table/table-row.ts
+++ packages/abstract-document/src/abstract-document/table/table-row.ts
@@ -1,12 +1,12 @@
 import { TableCell } from "./table-cell";
 
 export interface TableRow {
-  readonly children: Array<TableCell>;
+  readonly children: ReadonlyArray<TableCell>;
 }
 
 export interface TableRowProps {}
 
-export function create(props?: TableRowProps, children?: Array<TableCell>): TableRow {
+export function create(props?: TableRowProps, children?: ReadonlyArray<TableCell>): TableRow {
   const {} = props || {};
   return {
     children: children || [],
