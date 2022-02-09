4fc7e8c0966c4f8060c2d061daf2aeeee2a53b61
xpopy <3540971+xpopy@users.noreply.github.com>
2022-02-09 15:57:26 +0100

    Fix test

--- packages/abstract-document/src/abstract-document-exporters/__tests__/pdf/test-defs/absolute-position-header-and-footer.tsx
+++ packages/abstract-document/src/abstract-document-exporters/__tests__/pdf/test-defs/absolute-position-header-and-footer.tsx
@@ -55,5 +55,143 @@ export const test: ExportTestDef = {
       </Section>
     </AbstractDoc>
   ),
-  expectedPdfJson: {},
+  expectedPdfJson: {
+    formImage: {
+      Transcoder: "pdf2json@1.2.3 [https://github.com/modesty/pdf2json]",
+      Agency: "",
+      Id: {
+        AgencyId: "",
+        Name: "",
+        MC: false,
+        Max: 1,
+        Parent: "",
+      },
+      Pages: [
+        {
+          Height: 52.625,
+          HLines: [],
+          VLines: [],
+          Fills: [
+            {
+              x: 0,
+              y: 0,
+              w: 0,
+              h: 0,
+              clr: 1,
+            },
+          ],
+          Texts: [
+            {
+              x: -0.25,
+              y: -0.301,
+              w: 61.7,
+              sw: 0.32553125,
+              clr: 0,
+              A: "left",
+              R: [
+                {
+                  T: "I%20am%20a%20header",
+                  S: -1,
+                  TS: [0, 13, 0, 0],
+                },
+              ],
+            },
+            {
+              x: 33.569,
+              y: -0.301,
+              w: 5.56,
+              sw: 0.32553125,
+              clr: 0,
+              A: "left",
+              R: [
+                {
+                  T: "1",
+                  S: -1,
+                  TS: [0, 13, 0, 0],
+                },
+              ],
+            },
+            {
+              x: 33.916,
+              y: -0.301,
+              w: 2.78,
+              sw: 0.32553125,
+              clr: 0,
+              A: "left",
+              R: [
+                {
+                  T: "%2F",
+                  S: -1,
+                  TS: [0, 13, 0, 0],
+                },
+              ],
+            },
+            {
+              x: 34.09,
+              y: -0.301,
+              w: 5.56,
+              sw: 0.32553125,
+              clr: 0,
+              A: "left",
+              R: [
+                {
+                  T: "1",
+                  S: -1,
+                  TS: [0, 13, 0, 0],
+                },
+              ],
+            },
+            {
+              x: -0.25,
+              y: 49.629,
+              w: 56.14,
+              sw: 0.32553125,
+              clr: 0,
+              A: "left",
+              R: [
+                {
+                  T: "I%20am%20a%20footer",
+                  S: -1,
+                  TS: [0, 13, 0, 0],
+                },
+              ],
+            },
+            {
+              x: 1,
+              y: 50.879,
+              w: 56.14,
+              sw: 0.32553125,
+              clr: 0,
+              A: "left",
+              R: [
+                {
+                  T: "I%20am%20a%20footer",
+                  S: -1,
+                  TS: [0, 13, 0, 0],
+                },
+              ],
+            },
+            {
+              x: -0.25,
+              y: 1.144,
+              w: 43.91,
+              sw: 0.32553125,
+              clr: 0,
+              A: "left",
+              R: [
+                {
+                  T: "I%20am%20body",
+                  S: -1,
+                  TS: [0, 13, 0, 0],
+                },
+              ],
+            },
+          ],
+          Fields: [],
+          Boxsets: [],
+        },
+      ],
+      Width: 37.188,
+    },
+  },
 };
