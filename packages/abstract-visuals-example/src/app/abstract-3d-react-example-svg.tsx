4ace1433d3fed021acd683afb14d1b6153cc473c
timsve <tim.svensson@divid.se>
2024-06-04 16:22:03 +0200

    continue

--- packages/abstract-visuals-example/src/app/abstract-3d-react-example-svg.tsx
+++ packages/abstract-visuals-example/src/app/abstract-3d-react-example-svg.tsx
@@ -1,9 +1,36 @@
 import * as React from "react";
-// import * as A3D from "../../../abstract-3d/lib/index.js";
+import * as A3D from "../../../abstract-3d/lib/index.js";
 
 export function Abstract3DReactExample(): JSX.Element {
-  return <></>;
-  // return A3D.toReact({
-  //   scene: { center: A3D.vec3Zero, data: {}, groups: [], size: A3D.vec3Zero },
-  // });
+  return A3D.toReact({
+    scene: {
+      center: A3D.vec3Zero,
+      data: {},
+      groups: [
+        {
+          data: {},
+          groups: [],
+          pos: A3D.vec3Zero,
+          rot: A3D.vec3Zero,
+          meshes: [
+            {
+              geometry: A3D.boxGeometry(A3D.vec3(1, 1, 1)),
+              material: {
+                dxf: "1",
+                hover: "rgb(50,50,50)",
+                normal: "rgb(150,150,150)",
+                image: { type: "NoImage" },
+                imageType: "",
+                opacity: 1,
+                selected: "rgb(100,100,250)",
+                shininess: 100,
+                type: "Phong",
+              },
+            },
+          ],
+        },
+      ],
+      size: A3D.vec3Zero,
+    },
+  });
 }
