55410f0383a67a157bf3e318817e6fab29173784
timsve <tim.svensson@divid.se>
2024-09-17 15:39:44 +0200

    dummy2

--- packages/abstract-document/src/abstract-document-jsx/index.ts
+++ packages/abstract-document/src/abstract-document-jsx/index.ts
@@ -57,7 +57,6 @@ export function render(element: any): any {
   return (element.type as any)({ ...props, children });
 }
 
-//dummy
 function renderChildren(element: React.ReactElement<any>): any {
   if (!element.props || !element.props.children) {
     return [];
