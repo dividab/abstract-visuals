36f9120cdfc4759fc0f7d88357ee5c9938c9db17
Tim Svensson <tim.svensson@divid.se>
2024-11-28 15:14:41 +0100

    try-remove-hmtl

--- packages/abstract-3d/src/renderers/react/react-hotspot.tsx
+++ packages/abstract-3d/src/renderers/react/react-hotspot.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 // import { Html } from "@react-three/drei/web/Html";
-import { Html } from "@react-three/drei";
+// import { Html } from "@react-three/drei";
 import * as A3d from "../../abstract-3d";
 import { ReactMesh } from "./react-mesh";
 import { ReactMaterial } from "./react-material";
@@ -104,13 +104,13 @@ export function ReactHotSpot({
           <ReactMaterial id={h.id} isText={false} material={h.mesh.material} hoveredId={hoveredId} />
         </ReactMesh>
       </group>
-      {hotSpotTexts && text && (
+      {/* {hotSpotTexts && text && (
         <Html position={[hsPos.x, hsPos.y, hotSpotZAdjPos]} center>
           <div className={`air-states-container ${showHotSpotTexts ? "" : "air-states-container-hidden"}`}>
             <span className="air-states-text">{text}</span>
           </div>
         </Html>
-      )}
+      )} */}
     </>
   );
 }
