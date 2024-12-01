87b1182995cda665e41643d2885364da99839211
Tim Svensson <tim.svensson@divid.se>
2024-12-01 09:52:43 +0100

    vitest-workspace

--- vitest.workspace.js
+++ vitest.workspace.js
@@ -1,6 +1,6 @@
 import { defineWorkspace } from "vitest/config";
 
 export default defineWorkspace([
-  "./packages/abstract-document/vite.config.js",
-  "./packages/abstract-image/vite.config.js",
+  "./packages/abstract-document/vite.config.ts",
+  "./packages/abstract-image/vite.config.ts",
 ]);
