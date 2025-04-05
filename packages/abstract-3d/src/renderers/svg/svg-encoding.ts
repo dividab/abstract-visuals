107ed362e6a044140be965ff0bbf2f8f9682fb3b
Tim Svensson <tim.svensson@divid.se>
2025-04-05 14:55:16 +0200

    dummy

--- packages/abstract-3d/src/renderers/svg/svg-encoding.ts
+++ packages/abstract-3d/src/renderers/svg/svg-encoding.ts
@@ -1,4 +1,5 @@
 import { vec2, Vec2, vec2Add, vec2Scale } from "../../abstract-3d.js";
+//dummy
 
 export const svg = (width: number, height: number, children: string): string => {
   return `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 ${width.toFixed(0)} ${height.toFixed(
