efb802c1b3a8f24708eec507d4e63ceea57598e1
Tim Svensson <tim.svensson@divid.se>
2025-04-05 15:12:16 +0200

    try rotate first

--- packages/abstract-3d/src/renderers/svg/svg-encoding.ts
+++ packages/abstract-3d/src/renderers/svg/svg-encoding.ts
@@ -1,5 +1,4 @@
 import { vec2, Vec2, vec2Add, vec2Scale } from "../../abstract-3d.js";
-//dummy
 
 export const svg = (width: number, height: number, children: string): string => {
   return `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 ${width.toFixed(0)} ${height.toFixed(
@@ -34,14 +33,16 @@ export type EmbededImage =
 export const svgImage = (p: Vec2, size: Vec2, rot: number, data: EmbededImage): string => {
   const newSize = rotatedBoundingBox(size, rot);
   const half = vec2Scale(newSize, 0.5);
-  const transform = `${rotate(rot)}" ${transformOrigin(p, half)}`;
   return data.type === "url"
-    ? `<image x="${p.x.toFixed(0)}" y="${p.y.toFixed(0)}" transform="${transform}" width="${newSize.x.toFixed(
-        0
-      )}" height="${newSize.y.toFixed(0)}" href="${data.url}" />`
-    : `<svg x="${p.x.toFixed(0)}" y="${p.y.toFixed(0)}" transform="${transform}" width="${newSize.x.toFixed(
-        0
-      )}" height="${newSize.y.toFixed(0)}">${data.svg}</svg>`;
+    ? `<image x="${p.x.toFixed(0)}" y="${p.y.toFixed(0)}" transform="${rotate(rot)}" ${transformOrigin(
+        p,
+        half
+      )} width="${newSize.x.toFixed(0)}" height="${newSize.y.toFixed(0)}" href="${data.url}" />`
+    : `<svg width="${newSize.x.toFixed(0)}" height="${newSize.y.toFixed(0)}" transform="${rotate(rot)} ${translate(
+        vec2Add(p, half)
+      )} ${translate(vec2Scale(half, -1))}">${data.svg}</svg>
+
+  `;
 };
 
 const transformOrigin = (p: Vec2, half: Vec2): string =>
