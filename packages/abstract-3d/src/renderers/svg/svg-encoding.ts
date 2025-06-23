ac04f607de3daabc0d89ca0258c92bec235fd45c
Tim Svensson <tim.svensson@divid.se>
2025-06-23 13:18:27 +0200

    better check

--- packages/abstract-3d/src/renderers/svg/svg-encoding.ts
+++ packages/abstract-3d/src/renderers/svg/svg-encoding.ts
@@ -39,14 +39,15 @@ export const svgImage = (p: Vec2, size: Vec2, rot: number, data: EmbededImage): 
   const originalCenter = vec2(size.x / 2, size.y / 2);
   const rotatedCenter = vec2(newSize.x / 2, newSize.y / 2);
   const delta = vec2(originalCenter.x - rotatedCenter.x, originalCenter.y - rotatedCenter.y);
-  if (newSize.x === 0 || newSize.y === 0) {
+  const [newSizeX, newSizeY] = [newSize.x.toFixed(0), size.y.toFixed(0)];
+  if (newSizeX === "0" || newSizeY === "0") {
     return "";
   }
 
   return data.type === "url"
-    ? `<image width="${size.x.toFixed(0)}" height="${size.y.toFixed(0)}" x="${p.x.toFixed(0)}" y="${p.y.toFixed(
-        0
-      )}" transform="${rotate(rot)}" ${transformOrigin(p, half)}  href="${data.url}" />`
+    ? `<image width="${newSizeX}" height="${newSizeY}" x="${p.x.toFixed(0)}" y="${p.y.toFixed(0)}" transform="${rotate(
+        rot
+      )}" ${transformOrigin(p, half)}  href="${data.url}" />`
     : `<svg width="${newSize.x.toFixed(0)}" height="${newSize.y.toFixed(0)}" transform="${translate(p)} ${rotateAtPos(
         rot,
         half
