44ca3d2959d5a501e3778197a3faad7926ce8069
Tim Svensson <tim.svensson@divid.se>
2025-08-14 11:49:00 +0200

    revert as import2

--- packages/abstract-sheet/src/abstract-sheet-exporters/csv.ts
+++ packages/abstract-sheet/src/abstract-sheet-exporters/csv.ts
@@ -1,4 +1,4 @@
-import { Sheet2CSVOpts, utils } from "xlsx-js-style";
+import XLSX from "xlsx-js-style";
 import { AbstractSheet } from "../abstract-sheet/abstract-sheet.js";
 import { xlsxWorkSheet } from "./_xlsx-sheet.js";
 import { createStyle } from "./_style.js";
@@ -18,7 +18,7 @@ export type CsvOptions = {
 
 export function toCsv(as: AbstractSheet, options?: CsvOptions): ReadonlyArray<CsvFile> {
   const styles = Object.fromEntries(as.styles?.map((s) => [s.name, createStyle(s)]) ?? []);
-  const mappedOptions: Sheet2CSVOpts | undefined = options
+  const mappedOptions: XLSX.Sheet2CSVOpts | undefined = options
     ? {
         FS: options.separator,
         RS: options.rowSeparator,
@@ -32,6 +32,6 @@ export function toCsv(as: AbstractSheet, options?: CsvOptions): ReadonlyArray<Cs
     : undefined;
 
   return as.sheets.map(
-    (s): CsvFile => ({ name: s.name, csv: utils.sheet_to_csv(xlsxWorkSheet(s, styles), mappedOptions) })
+    (s): CsvFile => ({ name: s.name, csv: XLSX.utils.sheet_to_csv(xlsxWorkSheet(s, styles), mappedOptions) })
   );
 }
--- packages/abstract-sheet/src/abstract-sheet-exporters/xlsx.ts
+++ packages/abstract-sheet/src/abstract-sheet-exporters/xlsx.ts
@@ -1,10 +1,10 @@
-import { WorkSheet, WorkBook, write } from "xlsx-js-style";
+import XLSX from "xlsx-js-style";
 import { AbstractSheet } from "../abstract-sheet/abstract-sheet.js";
 import { xlsxWorkSheet } from "./_xlsx-sheet.js";
 import { createStyle } from "./_style.js";
 
 export function toXlsx(as: AbstractSheet): Buffer {
-  const sheets: Record<string, WorkSheet> = {};
+  const sheets: Record<string, XLSX.WorkSheet> = {};
   const sheetNames = Array<string>();
 
   const styles = Object.fromEntries(as.styles?.map((s) => [s.name, createStyle(s)]) ?? []);
@@ -13,6 +13,6 @@ export function toXlsx(as: AbstractSheet): Buffer {
     sheets[s.name] = xlsxWorkSheet(s, styles);
   }
 
-  const wb: WorkBook = { Sheets: sheets, SheetNames: Object.keys(sheets) };
-  return write(wb, { bookType: "xlsx", bookSST: false, type: "buffer" });
+  const wb: XLSX.WorkBook = { Sheets: sheets, SheetNames: Object.keys(sheets) };
+  return XLSX.write(wb, { bookType: "xlsx", bookSST: false, type: "buffer" });
 }
