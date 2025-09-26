9633cd02fef185b1bc2f8bfafbae960ddb04c598
Tim Svensson <tim.svensson@divid.se>
2025-09-26 07:15:13 +0200

    abstractDocsXml

--- packages/abstract-document/src/abstract-document-xml/abstract-doc-of-xml/abstract-doc-of-xml.ts
+++ packages/abstract-document/src/abstract-document-xml/abstract-doc-of-xml/abstract-doc-of-xml.ts
@@ -1,6 +1,47 @@
+import { exportToHTML5Blob as docxBlob } from "../../abstract-document-exporters/docx2/render.js";
+import { exportToHTML5Blob as pdfBlob } from "../../abstract-document-exporters/pdf/render.js";
+import { addResources, merge } from "../../abstract-document/abstract-doc.js";
 import { AbstractDoc } from "../../abstract-document/index.js";
+import { Resources } from "../../abstract-document/resources.js";
 import { ADCreatorFn, creators, propsCreators } from "./creator.js";
 import { parseHandlebarsXml, parseMustacheXml, type XmlElement } from "handlebars-xml";
+
+export type TemplateInput = {
+  readonly template: string;
+  readonly data: any;
+  readonly partials: Record<string, string>;
+};
+
+export type AbstractDoxXmlsResult =
+  | { readonly type: "Ok"; readonly value: Blob }
+  | { readonly type: "Err"; readonly error: string };
+
+export async function abstractDocsXml(
+  docInputs: ReadonlyArray<TemplateInput>,
+  format: "PDF" | "DOCX",
+  pdfKit: PDFKit.PDFDocument,
+  getResources: (imageUrls: Record<string, true>, fontFamilies: Record<string, true>) => Promise<Resources>
+): Promise<AbstractDoxXmlsResult> {
+  try {
+    const abstractDocs = Array<AbstractDoc.AbstractDoc>();
+    let imageUrls: Record<string, true> = {};
+    let fontFamilies: Record<string, true> = {};
+    for (const r of docInputs) {
+      const [ad, newImageUrls, newFontFamilies] = abstractDocXml(r.template, r.data, r.partials, "Handlebars");
+      abstractDocs.push(ad);
+      imageUrls = { ...imageUrls, ...newImageUrls };
+      fontFamilies = { ...fontFamilies, ...newFontFamilies };
+    }
+    const resources = await getResources(imageUrls, fontFamilies);
+    const combinedReport = addResources(merge(...abstractDocs), resources);
+    return {
+      type: "Ok",
+      value: format === "PDF" ? await pdfBlob(pdfKit, combinedReport) : await docxBlob(combinedReport),
+    };
+  } catch (e) {
+    return { type: "Err", error: typeof e === "string" ? e : e.message };
+  }
+}
 
 export function abstractDocXml(
   template: string,
@@ -39,7 +80,7 @@ function abstractDocXmlRecursive(
         const arrayedElements = ["header", "footer", "headerRows"];
         const childrenOnly = arrayedElements.findIndex((e) => e === childName) !== -1;
         const differentFirstPage = childElement.attributes.differentFirstPage;
-        if(differentFirstPage === "true" && childName === "header") {
+        if (differentFirstPage === "true" && childName === "header") {
           props.frontHeader = abstractDocXmlRecursive(creators, childElement, childrenOnly);
         } else if (differentFirstPage === "true" && childName === "footer") {
           props.frontFooter = abstractDocXmlRecursive(creators, childElement, childrenOnly);
