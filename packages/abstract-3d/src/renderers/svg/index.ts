eeb01f5a24dd76f85a8e2cd7a32927b18bdf141e
Tim Svensson <tim.svensson@divid.se>
2025-09-27 21:36:26 +0200

    clean up props2

--- packages/abstract-3d/src/renderers/svg/index.ts
+++ packages/abstract-3d/src/renderers/svg/index.ts
@@ -1,3 +1,3 @@
 export * from "./svg.js";
 export { type EmbededImage } from "./svg-encoding.js";
-export { type ImageDataUri } from "./svg-geometries/shared.js";
+export type { ImageDataUri, SvgOptions } from "./svg-geometries/shared.js";
