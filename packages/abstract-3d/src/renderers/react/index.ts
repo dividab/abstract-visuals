be04da11f7c3892261d24f2f07164531cf42c08e
Tim Svensson <tim.svensson@divid.se>
2025-09-29 10:16:31 +0200

    export type

--- packages/abstract-3d/src/renderers/react/index.ts
+++ packages/abstract-3d/src/renderers/react/index.ts
@@ -1,5 +1,5 @@
 export * from "./react.js";
-export type { ReactPopover } from "./react-scene.js";
+export type { ReactPopover, SceneMouseEvent } from "./react-scene.js";
 export type { HotSpotInfo } from "./react-hotspot.js";
 export type { CameraType, Camera, ControlsHelper } from "./react-camera.js";
 export type { MaterialState } from "./react-material.js";
