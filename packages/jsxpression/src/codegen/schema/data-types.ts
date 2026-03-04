30a26330ab8724bfd8f87a185ce7f5fbec1523ea
Rasmus Hallbäck <rasmus.hallback@divid.se>
2026-03-04 10:37:00 +0100

    test

--- packages/jsxpression/src/codegen/schema/data-types.ts
+++ packages/jsxpression/src/codegen/schema/data-types.ts
@@ -5,20 +5,17 @@ export function declareDataTypes(schema: Schema): string {
   let output = `declare global {\n`;
 
   if (schema.data) {
-    output += generateDataObjectComment(schema.data, "  ");
-    //output += `  const data: {\n`;
     Object.entries(schema.data).forEach(([dataKey, dataSchema]) => {
-      output += generateNestedPropertyJSDoc(dataKey, dataSchema, "    ");
+      output += generateNestedPropertyJSDoc(dataKey, dataSchema, "  ");
 
       const type = mapSchemaTypeToTypeScript(dataSchema);
 
       const indentedType = type.includes("\n") ? type.replace(/\n/g, "\n    ").replace(/\n {4}$/, "\n  ") : type;
       output += `  ${dataKey}: ${indentedType};\n`;
     });
-    output += `  };\n`;
   }
 
-  //output += `}\n\n`;
+  output += `}\n\n`;
   output += `export {};\n`;
 
   return output;
@@ -61,39 +58,6 @@ function generateNestedPropertyJSDoc(propertyName: string, propertySchema: any, 
   if (comments.length === 1 && !hasNestedProperties) {
     return `${indent}/** ${comments[0]} */\n`;
   }
-
-  let output = `${indent}/**\n`;
-  comments.forEach((comment) => {
-    if (comment === "") {
-      output += `${indent} *\n`;
-    } else {
-      output += `${indent} * ${comment}\n`;
-    }
-  });
-  output += `${indent} */\n`;
-
-  return output;
-}
-
-function generateDataObjectComment(dataSchema: any, indent: string = ""): string {
-  const comments: string[] = [];
-
-  comments.push("Data object for JSX expressions");
-  comments.push("");
-
-  Object.entries(dataSchema).forEach(([dataKey, schema]: [string, any]) => {
-    if (schema.description) {
-      comments.push(`@property ${dataKey} ${schema.description}`);
-    } else {
-      const desc =
-        dataKey.charAt(0).toUpperCase() +
-        dataKey
-          .slice(1)
-          .replace(/([A-Z])/g, " $1")
-          .toLowerCase();
-      comments.push(`@property ${dataKey} ${desc}`);
-    }
-  });
 
   let output = `${indent}/**\n`;
   comments.forEach((comment) => {
