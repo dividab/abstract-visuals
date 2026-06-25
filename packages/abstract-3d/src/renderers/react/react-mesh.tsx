f2eff01fd3eabe59c59865dd04c7482f49ebe542
Tim Svensson <tim.svensson@divid.se>
2026-06-25 20:53:17 +0200

    suspense fonts

--- packages/abstract-3d/src/renderers/react/react-mesh.tsx
+++ packages/abstract-3d/src/renderers/react/react-mesh.tsx
@@ -234,21 +234,23 @@ export function ReactMesh({
     case "Text": {
       const { pos, rot, text, fontSize } = mesh.geometry;
       return (
-        <Text
-          position={[pos.x, pos.y, pos.z]}
-          rotation={[rot?.x ?? 0, rot?.y ?? 0, rot?.z ?? 0]}
-          fontSize={fontSize}
-          textAlign="center"
-          letterSpacing={0.001}
-          lineHeight={1.5}
-          // outlineColor="rgb(255,255,255)"
-          // depthOffset={10}
-          // outlineWidth={3}
-          // outlineOpacity={1}
-        >
-          {children}
-          {text}
-        </Text>
+        <React.Suspense fallback={null}>
+          <Text
+            position={[pos.x, pos.y, pos.z]}
+            rotation={[rot?.x ?? 0, rot?.y ?? 0, rot?.z ?? 0]}
+            fontSize={fontSize}
+            textAlign="center"
+            letterSpacing={0.001}
+            lineHeight={1.5}
+            // outlineColor="rgb(255,255,255)"
+            // depthOffset={10}
+            // outlineWidth={3}
+            // outlineOpacity={1}
+          >
+            {children}
+            {text}
+          </Text>
+        </React.Suspense>
       );
     }
     case "Line": {
