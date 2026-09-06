eceeb12753ed0e5c694781fe4b380aa6655c5c0b
Tim Svensson <tim.svensson@divid.se>
2026-09-06 09:22:05 +0200

    activated no-throw-literal

--- oxlint.config.js
+++ oxlint.config.js
@@ -38,7 +38,6 @@ export default defineConfig({
     "typescript/no-unnecessary-condition": "off", // 164 errors
     "no-useless-return": "off", // 5 errors
     "typescript/ban-tslint-comment": "off", // 11 errors
-    "no-throw-literal": "off", // 4 errors
     "typescript/unified-signatures": "off", // 5 errors
     "no-bitwise": "off", // 38 errors
     "init-declarations": "off", // 14 errors
